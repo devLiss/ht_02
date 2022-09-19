7e10672127fd8f884deb135266c639a6587113e5
devliss <liseyna1@gmail.com>
2022-09-19 14:40:09 +0300

    changed blogNameValidation v3

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -3,7 +3,7 @@ import {CustomValidator} from "express-validator";
 const {body} = require("express-validator");
 
 const isValidUrl: CustomValidator = value => {
-    if(!/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/.test(value)){
+    if(!/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/.test(value) && value.length > 100){
         throw new Error('Неверный URL!')
     }
     else return true
@@ -13,4 +13,3 @@ export const blogNameValidation = body('name').trim(" ")
     .isLength({min:1, max:15}).withMessage('Поле не должно быть пустым и не должно превышать 15 символов')
 export const urlValidation =  body('youtubeUrl')
     .custom(isValidUrl)
-    .isLength({min:1, max:100}).withMessage('Поле не должно быть пустым и не должно превышать 100 символов')
